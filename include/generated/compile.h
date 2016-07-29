c711d67830573ed10bf13792d9e1f911883789b2
DroidManPK <pavankesha242@outlook.com>
2016-07-29 19:45:47 +0530

    Stock Permissive

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#6 SMP PREEMPT Fri Jul 29 18:59:30 IST 2016"
+#define LINUX_COMPILE_BY "pavan"
+#define LINUX_COMPILE_HOST "pavan-ROG550JK"
+#define LINUX_COMPILER "gcc version 4.9.4 20160401 (optimized) (UBERTC-4.9.4) "
